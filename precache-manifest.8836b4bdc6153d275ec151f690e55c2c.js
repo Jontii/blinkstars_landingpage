4429d7d0e73723f00d6ab1c5465d27732b224d7a
Jonathan Bodling <jonathan.bodling@teliacompany.com>
2020-03-06 19:33:31 +0100

    Updates

---
+++ precache-manifest.8836b4bdc6153d275ec151f690e55c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd12d98ba26efcc72b18d57622bcd39e",
+    "url": "/blinkstars/index.html"
+  },
+  {
+    "revision": "527909d966c02944aa4d",
+    "url": "/blinkstars/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e19d67ff2b3fa1a10b7a",
+    "url": "/blinkstars/static/js/2.29c16b7a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/blinkstars/static/js/2.29c16b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527909d966c02944aa4d",
+    "url": "/blinkstars/static/js/main.adf5a37d.chunk.js"
+  },
+  {
+    "revision": "55d8766109148120f476",
+    "url": "/blinkstars/static/js/runtime-main.a2409600.js"
+  }
+]);
